710ec755bb49e0bf855ede378f01162d190ffbd1
Andrii Charkivskyi <90054248+TopCharik@users.noreply.github.com>
2023-03-11 13:12:25 +0200

    Update docx-file-form.service.ts

--- src/app/docx-file-form/docx-file-form.service.ts
+++ src/app/docx-file-form/docx-file-form.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class DocxFileFormService {
-  private apiUrl = environment.apiUrl;
+  private apiUrl = "https://fileformapi.azurewebsites.net";
 
   constructor(private http: HttpClient) { }
 
